08efc42bbe1f37186301ebdcc73f796fc54b0251
ab3victoria <ab3victoria@gmail.com>
2022-09-22 16:14:22 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        lorem, ipsum dolor sit amet consecturet adispoidg elit.
+        Eous dolorius
+        animi paritcular disguisse mulla dolorum figut
+        minus mans
+        Adispiciti distinto loreanus kravit el mismk
+      </p>
+    </section>
+  )
+}
+
+export default About
